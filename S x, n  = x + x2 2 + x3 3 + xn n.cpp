a7464c730d287d104be4f59e11a6ec74b12651dc
ThienTranK32 <125123982+ThienTran23k@users.noreply.github.com>
2023-03-05 13:25:17 +0700

    Add files via upload

---
+++ S x, n  = x + x2 2 + x3 3 + xn n.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+
+using namespace std;
+
+float TinhSn(float x, int n)
+{	
+	if(n == 0)
+		return 0;
+	if(n == 1)
+		return x;
+	return (1 + x/n) * TinhSn(x, n - 1) - (x/n) * TinhSn(x, n - 2);
+	}
+	
+int main()
+{
+	float x;
+	int n;
+	cout << "\nNhap x: "; 
+	cin >> x;
+	cout << "\nNhap n: ";
+ 	cin >> n;
+	cout << "\nKet qua = " <<TinhSn(x, n) << endl;
+	return 0;
+}
+
